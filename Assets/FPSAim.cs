3d8d8883491412fa13f456d31af0422e6ae45b9c
Cameron Brown <cb12389@gmail.com>
2015-07-25 17:09:51 -0700

    First Checkin
    Learning Unity & C#

---
+++ Assets/FPSAim.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSAim : MonoBehaviour {
+    float mouseX;
+    float mouseY;
+    public bool InvertedMouse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mouseX += Input.GetAxis("Mouse X");
+        if (InvertedMouse)
+        {
+            mouseY += Input.GetAxis("Mouse Y");
+        }
+        else
+        {
+            mouseY -= Input.GetAxis("Mouse Y");
+        }
+
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+	}
+}
