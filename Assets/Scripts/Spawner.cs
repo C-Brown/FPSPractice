70611a4c2fd87db25fa4274b1837cdabe30c2aaa
Cameron Brown <cb12389@gmail.com>
2015-07-27 16:03:25 -0700

    Added Capsules "Zombie" Class and Spawner Class
    Testing dynamic spawning/creation of objects.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string[] names = new string[] { "stubbs", "rob", "white" };
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            Zombie z = new Zombie(names[i], Random.Range(10, 15));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+    public string Name;
+    public int brainsEaten;
+    public int hitPoints;
+    GameObject ZombieMesh;
+
+    public Zombie(string Name, int hitPoints)
+    {
+        this.Name = Name;
+        brainsEaten = 0;
+        this.hitPoints = hitPoints;
+        ZombieMesh = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        Vector3 pos = new Vector3(
+            Random.Range(-10, 10), 1f,
+            Random.Range(-10, 10));
+        ZombieMesh.transform.position = pos;
+    }
+}
