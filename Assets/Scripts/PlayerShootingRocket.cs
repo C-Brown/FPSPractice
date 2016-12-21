4c7c5c904b21add63b542ed01d7b6bf82d744eb6
Cameron Brown <cb12389@gmail.com>
2016-12-21 12:54:43 -0600

    Added Photon networking? Random late update
    Made changes sooo long ago.. just updating the folder.

---
+++ Assets/Scripts/PlayerShootingRocket.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShootingRocket : MonoBehaviour {
+
+    public Rigidbody rocket;
+    public float launchPower = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody clone;
+
+            clone = (Rigidbody)Instantiate(rocket, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * launchPower);
+        }
+	}
+}
---
+++ Assets/Scripts/RocketScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketScript : MonoBehaviour {
+
+    public float radius = 5;
+    public float power = 10;
+    public float explosiveLift = 1;
+
+	// Use this for initialization
+	void Start () {
+
+        Destroy(gameObject, 3);
+
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision collision) {
+        Vector3 rocketOrigin = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(rocketOrigin, radius);
+
+        foreach(Collider hit in colliders) {
+            if (hit.GetComponent<Rigidbody>())
+            {
+                hit.GetComponent<Rigidbody>().AddExplosionForce(power, rocketOrigin, explosiveLift);
+                Destroy(gameObject);
+            }
+        }
+	}
+}
